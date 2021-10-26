5a83ebfffa46cf167aa8f2c2bbefcae327c502b2
metallims <82535466+x5d@users.noreply.github.com>
2021-10-25 21:37:23 -0700

    Added other space-ids for those who wish to wipe console

--- tool.py
+++ tool.py
@@ -58,6 +58,9 @@ def request_delete(authorization):
         'Ubi-LocaleCode' : 'en-us',
     }
     params = {
+        #uplay - 5172a557-50b5-4665-b7db-e3f2e8c5041d
+        #xbox - 98a601e5-ca91-4440-b1c5-753f601a2c90
+        #playstation - 05bfb3f7-6c21-4c42-be1f-97a33fb5cf66
         'spaceId' : '5172a557-50b5-4665-b7db-e3f2e8c5041d'
     }
     print(Fore.GREEN + '[+] Ready')
